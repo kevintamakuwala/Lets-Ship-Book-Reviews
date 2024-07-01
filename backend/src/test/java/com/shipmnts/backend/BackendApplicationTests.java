3656ea0a0ac770ecdffc0f57cf8d11b717e7048a
kevintamakuwala <kevintamakuwala16@gmail.com>
2024-07-01 14:09:22 +0530

    Spring Boot initial project setup

---
+++ backend/src/test/java/com/shipmnts/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shipmnts.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
